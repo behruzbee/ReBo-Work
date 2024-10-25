8d11567abc16f13c9065ec374facc03dad2cdaac
Behruz <behruz@bictory.io>
2024-10-25 21:03:22 +0500

    deploy 1.9

--- src/shared/api/base-api.ts
+++ src/shared/api/base-api.ts
@@ -1,6 +1,5 @@
-import axios from 'axios';
+import axios from 'axios'
 
-// Замените 'localhost' на ваш IP-адрес
 export const instance = axios.create({
-  baseURL: 'http://45.138.158.146:3000' // ваш IP-адрес
-});
+  baseURL: 'http://localhost:3000'
+})
